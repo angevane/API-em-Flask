c64b5c19828469563eec8a823eadb072c1702623
Angevane <ladutra144@gmail.com>
2025-12-14 18:31:29 -0300

    feat: cadastrando livro (POST)

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ app = Flask(__name__)
 livros = []
 contador_id = 1
 
+
+@app.route("/livros", methods=["POST"])
+def criar_livro():
+    global contador_id
+    dados = request.json
+
+    livro = {
+        "id": contador_id,
+        "titulo": dados["titulo"],
+        "autor": dados["autor"],
+        "ano": dados["ano"]
+    }
+
+    livros.append(livro)
+    contador_id += 1
+
+    return jsonify(livro), 201
